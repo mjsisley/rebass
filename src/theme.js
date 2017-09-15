a28c2634197cf0ac27a0ae78575d50109dde46ea
Matthew Sisley <mjsisley@gmail.com>
2017-09-14 20:09:28 -0700

    switch palx reference

--- src/theme.js
+++ src/theme.js
@@ -1,66 +1,38 @@
-import palx from 'palx'
+import palx from "@mjsisley/palx";
 
-export const breakpoints = [
-  32,
-  48,
-  64,
-  80
-]
+export const breakpoints = [32, 48, 64, 80];
 
-export const space = [
-  0,
-  4,
-  8,
-  16,
-  32,
-  64,
-  128,
-]
+export const space = [0, 4, 8, 16, 32, 64, 128];
 
-export const fontSizes = [
-  12,
-  14,
-  16,
-  20,
-  24,
-  32,
-  48,
-  64,
-  72,
-  96
-]
+export const fontSizes = [12, 14, 16, 20, 24, 32, 48, 64, 72, 96];
 
-export const weights = [
-  400,
-  700
-]
+export const weights = [400, 700];
 
-const palette = palx('#07c')
+const palette = palx("#07c");
 
-const flattened = Object.keys(palette)
-  .reduce((a, key) => {
-    const value = palette[key]
-    if (Array.isArray(value)) {
-      a[key] = value[5]
-      value.forEach((val, i) => {
-        a[key + i] = val
-      })
-    } else {
-      a[key] = value
-    }
-    return a
-  }, {})
+const flattened = Object.keys(palette).reduce((a, key) => {
+  const value = palette[key];
+  if (Array.isArray(value)) {
+    a[key] = value[5];
+    value.forEach((val, i) => {
+      a[key + i] = val;
+    });
+  } else {
+    a[key] = value;
+  }
+  return a;
+}, {});
 
 // todo: flatten
 
 export const colors = Object.assign({}, flattened, {
-  black: '#000',
-  white: '#fff'
-})
+  black: "#000",
+  white: "#fff"
+});
 
-export const radius = 4
-export const font = `-apple-system, BlinkMacSystemFont, sans-serif`
-export const monospace = '"SF Mono", "Roboto Mono", Menlo, monospace'
+export const radius = 4;
+export const font = `-apple-system, BlinkMacSystemFont, sans-serif`;
+export const monospace = '"SF Mono", "Roboto Mono", Menlo, monospace';
 
 export default {
   breakpoints,
@@ -70,5 +42,5 @@ export default {
   font,
   monospace,
   colors,
-  radius,
-}
+  radius
+};
